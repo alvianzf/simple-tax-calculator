b625328c0f666412fc1f2e84b2313692454f9d09
alvianzf <alvianzf@gmail.com>
2026-02-10 14:20:36 +0700

    feat(ui): Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Simple Tax<span>.</span></h1>
+      <p class="subtitle">Calculate your contribution to the nation (sadly).</p>
+    </header>
+  );
+};
+
+export default Header;
